4036b4f0f2cfde92741fbe7077c392663db5a6a8
Brandon Orther <brandon@uptrend.tech>
2018-04-16 20:38:55 -0700

    chore: Remove custom jest testMatch config
    No longer needed since my last commit moved fictures into src/utils/test/fixtures.js

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 const {jest: jestConfig} = require('kcd-scripts/config')
 
 module.exports = Object.assign(jestConfig, {
-  testMatch: ['**/__tests__/**/*.test.js?(x)'],
+  // TODO remove these
   testPathIgnorePatterns: [
     '<rootDir>/src/resource',
     '<rootDir>/src/entities/__tests__/middleware.test.js',
