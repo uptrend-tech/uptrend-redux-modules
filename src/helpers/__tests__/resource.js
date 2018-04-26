a8c756c4a9bfb38ca2f59afc8e28b2d0600ed355
Brandon Orther <brandon@uptrend.tech>
2018-04-26 06:22:34 -0700

    feat(resource): WIP Add createResource w/ action test
    Still need to add selectors tests and implement other resource request types.

--- src/helpers/__tests__/resource.js
+++ src/helpers/__tests__/resource.js
@@ -1,6 +1,61 @@
+import cases from 'jest-in-case'
+import {schema} from 'normalizr'
+import * as actions from '../../resource/actions'
 import createResourceHelpers from '../resource'
 import {createResource, createEntities} from '../../'
-import {getSchemas} from '../../utils/test/fixtures'
+// import {getSchemas} from '../../utils/test/fixtures'
+
+// --
+// -- Schemas
+// --
+
+const userSchema = new schema.Entity('user', {}, {idAttribute: 'uuid'})
+
+const teamSchema = new schema.Entity(
+  'team',
+  {owner: userSchema, members: [userSchema]},
+  {idAttribute: 'id'},
+)
+
+const getSchemas = () => ({
+  team: teamSchema,
+  user: userSchema,
+})
+
+// --
+// -- State
+// --
+
+const getEntitiesState = () => ({
+  team: {
+    1: {
+      id: 1,
+      name: 'Team 1',
+    },
+    2: {
+      id: 2,
+      name: 'Team 2',
+    },
+    3: {
+      id: 3,
+      name: 'Team 3',
+    },
+  },
+  user: {
+    aaa: {
+      uuid: 'one',
+      name: 'orther',
+    },
+    bbb: {
+      uuid: 'two',
+      name: 'benjamin',
+    },
+    ccc: {
+      uuid: 'three',
+      name: 'dylan',
+    },
+  },
+})
 
 const createHelpers = () => {
   const entities = createEntities({isDevEnv: false, schemas: getSchemas()})
@@ -19,4 +74,47 @@ describe('createResourceHelpers', () => {
     expect(resourceHelpers.resourceListRead).toBeDefined()
     expect(resourceHelpers.resourceUpdate).toBeDefined()
   })
+})
+
+describe('resourceCreate', () => {
+  const {resourceCreate} = createHelpers()
+
+  cases(
+    'action',
+    opts => {
+      const {action} = resourceCreate(opts.resourcePath, opts.entityType)
+      expect(action(opts.payload)).toEqual(
+        actions.resourceCreateRequest(
+          opts.resourcePath,
+          opts.payload,
+          opts.entityType,
+        ),
+      )
+    },
+    {
+      'resourcePath ONLY (no entityType)': {
+        resourcePath: 'resourcePath',
+        entityType: 'schemaName',
+        payload: {id: 1, title: 'test'},
+      },
+      'resourcePath & entityType': {
+        resourcePath: 'resourcePath',
+        entityType: undefined,
+        payload: {id: 1, title: 'test'},
+      },
+    },
+  )
+
+  // const mapDispatchToProps = {
+  //   search: params => search.action(params), // = resourceListCreateRequest(`user/member`, data, 'member)
+  // }
+
+  // test('selectors', () => {
+  //   const mapStateToProps = state => ({
+  //     memberListDone: search.selectors.done(state),
+  //     memberListFailed: search.selectors.rejected(state),
+  //     memberListPending: search.selectors.pending(state),
+  //     memberList: search.selectors.result(state),
+  //   })
+  // })
 })
--- src/helpers/resource.js
+++ src/helpers/resource.js
@@ -1,13 +1,13 @@
 import {done, fulfilled, pending, rejected} from 'redux-saga-thunk'
 
-const partialStatusActions = name => {
-  return {
-    done: state => done(state, name),
-    fulfilled: state => fulfilled(state, name),
-    pending: state => pending(state, name),
-    rejected: state => rejected(state, name),
-  }
-}
+// const partialStatusActions = name => {
+//   return {
+//     done: state => done(state, name),
+//     fulfilled: state => fulfilled(state, name),
+//     pending: state => pending(state, name),
+//     rejected: state => rejected(state, name),
+//   }
+// }
 
 export default ({entities, resource}) => {
   const {
@@ -19,25 +19,70 @@ export default ({entities, resource}) => {
     // resourceUpdateRequest,
   } = resource.actions
 
-  const resourceCreate = (resourcePath, entityType) => {
-    const resourceSelect = state =>
-      resource.selectors.getDetail(state, resourcePath)
+  const createHelper = (
+    requestAction,
+    thunkNameFn,
+    resourceSelector,
+    entitiesSelector,
+  ) => (resourcePath, entityType) => {
+    const thunkName = thunkNameFn(resourcePath)
+    const action = data => requestAction(resourcePath, data, entityType)
+    const resourceSelect = state => resourceSelector(state, resourcePath)
+    const resultsSelect = state => {
+      const detail = resourceSelect(state)
+      // TODO in future be able to check if actually normalized
+      return entityType ? entitiesSelector(state, entityType, detail) : detail
+    }
 
     return {
-      action: data => resourceCreateRequest(resourcePath, data, entityType),
+      action,
       selectors: {
-        ...partialStatusActions(`${resourcePath}Create`),
+        done: state => done(state, thunkName),
+        fulfilled: state => fulfilled(state, thunkName),
+        // eslint-disable-next-line jest/no-disabled-tests
+        pending: state => pending(state, thunkName),
+        rejected: state => rejected(state, thunkName),
         resource: resourceSelect,
-        result: state => {
-          const detail = resourceSelect(state)
-          if (entityType) {
-            return entities.selectors.getDetail(state, entityType, detail)
-          }
-          return detail
-        },
+        result: resultsSelect,
       },
     }
   }
+
+  // const resourceCreate = (resourcePath, entityType) => {
+  //   const action = data => resourceCreateRequest(resourcePath, data, entityType)
+  //   const thunkName = `${resourcePath}Create`
+
+  //   const resourceSelect = state =>
+  //     resource.selectors.getDetail(state, resourcePath)
+
+  //   const resultsSelect = state => {
+  //     const detail = resourceSelect(state)
+  //     if (entityType) {
+  //       return entities.selectors.getDetail(state, entityType, detail)
+  //     }
+  //     return detail
+  //   }
+
+  //   return {
+  //     action,
+  //     selectors: {
+  //       done: state => done(state, thunkName),
+  //       fulfilled: state => fulfilled(state, thunkName),
+  //       // eslint-disable-next-line jest/no-disabled-tests
+  //       pending: state => pending(state, thunkName),
+  //       rejected: state => rejected(state, thunkName),
+  //       resource: resourceSelect,
+  //       result: resultsSelect,
+  //     },
+  //   }
+  // }
+
+  const resourceCreate = createHelper(
+    resourceCreateRequest,
+    resourcePath => `${resourcePath}Create`,
+    resource.selectors.getDetail,
+    entities.selectors.getDetail,
+  )
 
   return {
     resourceCreate,
