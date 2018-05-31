c49a41a10c0acc402cd2cc92555fc8ad02e10574
Brandon Orther <brandon@uptrend.tech>
2018-05-30 18:19:11 -0700

    chore: Config Jest to require *.test.js file ext
    I keep running into situations where I want to make special helper or
    util configs for tests but when I add a file for that Jest failes
    because there aren't test assertions. I rather just require .test.js
    than put those test helper/util files outside of the `__tests__` folder.

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ const {jest: jestConfig} = require('uptrend-scripts/config')
 
 module.exports = Object.assign(jestConfig, {
   // collectCoverage: true,
-  coverageThreshold: undefined,
+  coverageThreshold: undefined, // TODO get 100% coverage and REMOVE this
+  testMatch: ['**/__tests__/**/*.test.+(js|jsx|ts|tsx)'],
 })
