9e06f3c8b0291e12cbdff290ea7f921a1c7d5143
Brandon Orther <brandon@uptrend.tech>
2018-06-20 08:32:08 -0700

    feat: Update resource to not auto camecase UUIDs (#41)

---
+++ src/utils/__tests__/index.test.js
@@ -0,0 +1,33 @@
+import {camelKeys, snakeKeys} from '../index'
+
+/* eslint-disable camelcase */
+describe('camelKeys', () => {
+  test.each`
+    source                | expected
+    ${{test_one: 1}}      | ${{testOne: 1}}
+    ${{'test-two': 2}}    | ${{'test-two': 2}}
+    ${{'a1b2C-three': 3}} | ${{'a1b2C-three': 3}}
+    ${{'a1b2C-Four': 4}}  | ${{'a1b2C-Four': 4}}
+  `(
+    'returns $expected when camelKeys is passed $source',
+    ({source, expected}) => {
+      expect(camelKeys(source)).toEqual(expected)
+    },
+  )
+})
+
+describe('snakeKeys', () => {
+  test.each`
+    source                | expected
+    ${{testOne: 1}}       | ${{test_one: 1}}
+    ${{'test-two': 2}}    | ${{'test-two': 2}}
+    ${{'a1b2C-three': 3}} | ${{'a1b2C-three': 3}}
+    ${{'a1b2C-Four': 4}}  | ${{'a1b2C-Four': 4}}
+  `(
+    'returns $expected when snakeKeys is passed $source',
+    ({source, expected}) => {
+      expect(snakeKeys(source)).toEqual(expected)
+    },
+  )
+})
+/* eslint-enable camelcase */
