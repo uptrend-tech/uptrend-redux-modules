563b2a79ce91558d1db693db2ead69b9dea69496
Brandon Orther <brandon@uptrend.tech>
2018-12-05 17:38:32 -0800

    build(babel): Add @babel/helpers & use in jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,14 @@
-const {jest: jestConfig} = require('uptrend-scripts/config')
+const jestConfig = require('uptrend-scripts/config').jest
+const babelHelpersList = require('@babel/helpers').list
 
 module.exports = Object.assign(jestConfig, {
+  // roots: ['.'],
+  // testEnvironment: 'jsdom',
   coverageThreshold: undefined, // TODO get 100% coverage and REMOVE this
+  moduleNameMapper: babelHelpersList.reduce((aliasMap, helper) => {
+    aliasMap[
+      `@babel/runtime/helpers/esm/${helper}`
+    ] = `@babel/runtime/helpers/${helper}`
+    return aliasMap
+  }, {}),
 })
